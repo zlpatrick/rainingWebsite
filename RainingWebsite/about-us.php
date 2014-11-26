247b2e548a218955f9240049b7770cd3e6961258
Patrick <patrick.zhang@autodesk.com>
2014-11-26 17:47:12 +0800

    about page

--- RainingWebsite/about-us.php
+++ RainingWebsite/about-us.php
@@ -1,12 +1,57 @@
 <html>
 <head>
-<title>关于我们  | 宇威科技</title>
+<title>关于我们 | 宇威科技</title>
 <?php include 'common.php';?>
 </head>
 
 <body>
 	<?php include 'nav.php';?>
 
+	<div class="container" style="margin-top: 50px;">
+		<ul class="breadcrumb" style="margin-top: 20px;background-color:white">
+			<li><span class="glyphicon glyphicon-home"></span> &nbsp;<a href="index.php" class="questionBlue">首页</a><span
+				class="divider"></span></li>
+			<li class="active">关于我们</li>
+		</ul>
+
+		<div class="row">
+			<div class="col-md-4">
+				<img src="img/about-2.jpg" class="img-responsive" />
+			</div>
+			<div class="col-md-8" style="line-height: 27px">
+				<p>公司技术团队有专业的软件外包开发团队和工业自动化控制专家，具有丰富的服务国内外一流大客户的软件开发服务经验,以及各类工业控制系统集成和开发经验，我们拥有管理解决方案设计与规划、软件系统设计与技术实现、信息系统实施与培训、工业自动化控制系统集成和开发四大专业能力；通过具有多年企业实际运作经验的系统分析员，具有专业知识和实践经验丰富的项目实施人员，为客户提供先进、易用、实用的软件产品及稳定、快速、优质的技术支持。</p>
+				<p>宇威信息秉承“诚信、专业、创新、惜时、协作、分享”的宗旨，注重员工服务意识的提升和人才的培养，以先进的技术、优质的产品和细心的服务，为客户提供最满意的服务。坚持务实的工作作风，构建以软件研发为依托，行业应用解决方案为核心的信息系统集成公司。</p>
+			</div>
+		</div>
+		<hr>
+		
+		<div class="row">
+			<div class="col-md-8" style="line-height: 27px">
+				<p>西安宇威信息科技有限公司研发团队成立于2005年，2010年注册公司。是一家专业的综合性软件服务和控制系统供应商，可以为客户及合作伙伴提供软件开发、软件产品、企业及行业管理信息化解决方案研发、工业自动控制系统解决方案、农业和家居物联网系统等专业服务。</p>
+				<p>公司创始于西安高新区，拥有专业的研发团队，我们立足西北，服务国内外客户。经过十年的踏实耕耘，艰苦积累，正逐步发展成为西安规模较大、自主研发能力最强、发展潜力最快、最具专业化水准的软件开发和工业自动化控制系统公司之一。</p>
+			</div>
+			<div class="col-md-4">
+				<img src="img/about-1.jpg" class="img-responsive" />
+			</div>
+		</div>
+		<hr>
+
+		<div style="margin-top: 20px">
+			<div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-fire"></span> &nbsp;拥有10年的软硬结合软件咨询服务经验</div>
+			<div style="color: grey; padding-left: 20px; margin-bottom: 20px">
+				10 years of Management Software Consulting Service Experience</div>
+			<div class="alert alert-info" role="alert"><span class="glyphicon glyphicon-fire"></span> &nbsp;拥有5年的产品开发与推广经验</div>
+			<div style="color: grey; padding-left: 20px; margin-bottom: 20px">5
+				years of Software production R&D and Spend Experience</div>
+			<div class="alert alert-warning" role="alert"><span class="glyphicon glyphicon-fire"></span> &nbsp;拥有8年的自动化控制、PDA、条码、GPS等硬件结合开发经验</div>
+			<div style="color: grey; padding-left: 20px; margin-bottom: 20px">8
+				years of Hardware development with intelligent control Mobile PDA
+				Barcode Experience</div>
+			<div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-fire"></span> &nbsp;拥有16个软件著作专利登记和软件产品</div>
+			<div style="color: grey; padding-left: 20px; margin-bottom: 20px">Over
+				16 Patents Registration of Software and Software Production</div>
+		</div>
+	</div>
 
 	<?php include 'footer.php';?>
 </body>
